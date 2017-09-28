95a1a6aeaf3a79feefcb6d5034cb2821f2de6108
tamas.a.kiss <Zorbo>
2017-09-28 17:12:51 +0200

    Adwenture 2.0

--- src/com/tamaskiss/Location.java
+++ src/com/tamaskiss/Location.java
@@ -9,16 +9,21 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exist;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exist  = new HashMap<>();
-        this.exist.put("Q",0);
+        if (exits != null){
+            this.exist = new HashMap<>(exits);
+        } else {
+            this.exist = new HashMap<>();
+        }
+
+        this.exist.put("Q", 0);
     }
 
-    public void addExit(String direction, int location){
-        exist.put(direction,location);
-    }
+//    public void addExit(String direction, int location){
+//        exist.put(direction,location);
+//    }
 
     public int getLocationID() {
         return locationID;
--- src/com/tamaskiss/Main.java
+++ src/com/tamaskiss/Main.java
@@ -6,39 +6,51 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static Map<Integer, Location> locations = new HashMap<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0,"You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1,"You are standing at the end of the road before a small brick building"));
-        locations.put(2, new Location(2,"You are at the top of a hill"));
-        locations.put(3, new Location(3,"You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4,"You are in a valley beside a spring"));
-        locations.put(5, new Location(5,"You are in the forest"));
+        Map<String, Integer> tempExits = new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExits));
 
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-        locations.get(1).addExit("N",5);
-      //  locations.get(1).addExit("Q",0);
+        tempExits = new HashMap<>();
+        tempExits.put("W", 2);
+        tempExits.put("E", 3);
+        tempExits.put("S", 4);
+        tempExits.put("N", 5);
+        locations.put(1, new Location(1, "You are standing at the end of the road before a small brick building", tempExits));
 
-        locations.get(2).addExit("N",5);
-     //   locations.get(2).addExit("Q",0);
+        //  tempExits.put("Q",0);
 
-        locations.get(3).addExit("W",1);
-     //   locations.get(3).addExit("Q",0);
+        tempExits = new HashMap<>();
+        tempExits.put("N", 5);
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExits));
 
-        locations.get(4).addExit("N",1);
-        locations.get(4).addExit("W",2);
-        //  locations.get(4).addExit("Q",0);
+        //   tempExits.put("Q",0);
 
-        locations.get(5).addExit("S",1);
-        locations.get(5).addExit("W",2);
-     //   locations.get(5).addExit("Q",0);
+        tempExits = new HashMap<>();
+        tempExits.put("W", 1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExits));
 
-        Map<String, String > vocabulary = new HashMap<>();
+        //   tempExits.put("Q",0);
+
+        tempExits = new HashMap<>();
+        tempExits.put("N", 1);
+        tempExits.put("W", 2);
+        locations.put(4, new Location(4, "You are in a valley beside a spring", tempExits));
+
+        //  tempExits.put("Q",0);
+
+        tempExits = new HashMap<>();
+        tempExits.put("S", 1);
+        tempExits.put("W", 2);
+        locations.put(5, new Location(5, "You are in the forest", tempExits));
+
+        //   tempExits.put("Q",0);
+
+        Map<String, String> vocabulary = new HashMap<>();
         vocabulary.put("QUIT", "Q");
         vocabulary.put("NORTH", "N");
         vocabulary.put("SOUTH", "S");
@@ -46,34 +58,33 @@ public class Main {
         vocabulary.put("EAST", "E");
 
         int loc = 1;
-        while (true){
+        while (true) {
             System.out.println(locations.get(loc).getDescription());
-            if (loc == 0){
+            tempExits.remove("S");
+            if (loc == 0) {
                 break;
             }
             Map<String, Integer> exits = locations.get(loc).getExist();
             System.out.println("Available areas are ");
-            for (String exit : exits.keySet()){
+            for (String exit : exits.keySet()) {
                 System.out.print(exit + ", ");
             }
             System.out.println();
 
             String direction = scanner.nextLine().toUpperCase();
 
-            if (direction.length() > 1){
+            if (direction.length() > 1) {
                 String[] words = direction.split(" ");
 
-                for (String i : words){
-                    if (vocabulary.containsKey(i)){
+                for (String i : words) {
+                    if (vocabulary.containsKey(i)) {
                         direction = vocabulary.get(i);
                         break;
                     }
-
                 }
             }
 
-
-            if (exits.containsKey(direction)){
+            if (exits.containsKey(direction)) {
                 loc = exits.get(direction);
             } else {
                 System.out.println("You cannot go in that direction");
