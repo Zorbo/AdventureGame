160ea61109fa90fd9b363f9f674cb2c3208a606f
tamas.a.kiss <Zorbo>
2017-09-28 12:17:35 +0200

    Adwenture first commit

---
+++ src/com/tamaskiss/Location.java
@@ -0,0 +1,34 @@
+package com.tamaskiss;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exist;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exist  = new HashMap<>();
+        this.exist.put("Q",0);
+    }
+
+    public void addExit(String direction, int location){
+        exist.put(direction,location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExist() {
+        return new HashMap<>(exist);
+    }
+}
